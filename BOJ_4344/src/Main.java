633de111f613eb9a4c607e736529a04bfd82afdc
tico <lours2021@gmail.com>
2020-10-26 18:14:29 +0900

    백준 4344, 4673

---
+++ BOJ_4344/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int C = scan.nextInt();
+        for (int i = 0; i < C; i++) {
+            int N = scan.nextInt();
+            int[] scores = new int[N];
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                scores[j] = scan.nextInt();
+                sum += scores[j];
+            }
+            double avg = (double)sum / N;
+            int count = 0;
+            for (int score : scores) {
+                if (score > avg) {
+                    count++;
+                }
+            }
+            System.out.printf("%.3f%%\n", (double)count/N*100);
+        }
+    }
+}
---
+++ BOJ_4673/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    final static int MAX_NUMBER = 10000;
+    static boolean[] dp = new boolean[MAX_NUMBER + 1];
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            if (isSelfNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isSelfNumber(int N) {
+        if (getGeneratorNumber(N) < MAX_NUMBER) {
+            dp[getGeneratorNumber(N)] = true;
+        }
+        return !dp[N];
+    }
+
+    public static int getGeneratorNumber(int N) {
+        int answer = N;
+        while (N > 0) {
+            answer += N % 10;
+            N /= 10;
+        }
+        return answer;
+    }
+}
