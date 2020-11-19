5248929e020972b5837168a4cba383b26359ac1e
tico <lours2021@gmail.com>
2020-11-19 18:10:38 +0900

    백준 3009 풀이 전 커밋

---
+++ BOJ_3009/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt(), y1 = sc.nextInt();
+        int x2 = sc.nextInt(), y2 = sc.nextInt();
+        int x3 = sc.nextInt(), y3 = sc.nextInt();
+        int x4 = 0, y4 = 0;
+        System.out.println(x4 + " " + y4);
+    }
+}
